2b08860539e47d8b1e4dcc52a76aa68bc7a2e2db
Anne Thor <anne.thor@gmail.com>
2018-10-14 12:02:55 -0700

    WIP deploy to production

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+const appConfig = require("./config/main-config.js");
+const routeConfig = require("./config/route-config.js");
+
+appConfig.init();
+routeConfig.init(app);
+
+module.exports = app;
+
+//this file should hole the routes & respective handlers
+//middleware, app configuration, error handling, etc
